20ea3d48bab095348dcdb0165c5343b8d4fdb471
Артем Шевелев <artemsevelev@Artems-MacBook-Pro.local>
2021-11-25 03:21:24 +0200

    final version of weather app

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+import CityWiseScreen from './Screens/CityWise';
+import CurrentScreen from './Screens/Current';
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import  MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const Tab = createMaterialBottomTabNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Tab.Navigator labeled={false} barStyle={{ backgroundColor: '#FFA07A' }} activeColor="black" >
+      <Tab.Screen name="Current" component={CurrentScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26}/>
+        ),
+    }}/>
+      <Tab.Screen name="CityWise" component={CityWiseScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="city" color={color} size={26}/>
+        ),
+    }}/>
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run:
+1. Set evironment (Expo CLI) lINK: https://reactnative.dev/docs/environment-setup
+2. expo start
+git in <img width="1243" alt="Screen Shot 2021-11-25 at 2 58 14" src="https://user-images.githubusercontent.com/74210822/143334128-350fbd4d-0bd3-46f5-88e7-9d26200eeb4a.png">
+<img width="1250" alt="Screen Shot 2021-11-25 at 2 58 56" src="https://user-images.githubusercontent.com/74210822/143334131-ff562ba6-5d92-4a0d-9dcf-c2c34947ca14.png">
